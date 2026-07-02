16724a662adc14b67b5e37cbfdf4d13a8fc19dda
Manya Singh <manyassingh126@gmail.com>
2026-07-02 19:29:21 +0530

    connect MongoDB Atlas to express server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Rent & Flatmate Finder API is running');
