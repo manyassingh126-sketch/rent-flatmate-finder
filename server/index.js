985f08e50c3d52a06257c4d9e01b8a921d7d9c3e
Manya Singh <manyassingh126@gmail.com>
2026-07-02 20:19:54 +0530

    Add suthentication : register , login , JWt , role-based middlewqare

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/auth', require('./routes/authRoutes'));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
